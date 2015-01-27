41ff2f0fd61330a7f6733603bd2d82ca3769cb7b
trzytematyczna <monikrakoczy@gmail.com>
2015-01-27 19:14:37 +0100

    reader & rssi converter

--- app/src/main/java/com/agh/eis/mralucp/beacontrilateration/CSVReader.java
+++ app/src/main/java/com/agh/eis/mralucp/beacontrilateration/CSVReader.java
@@ -1,7 +1,50 @@
 package com.agh.eis.mralucp.beacontrilateration;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
 /**
  * Created by Lucp on 2015-01-27.
  */
 public class CSVReader {
+
+
+    LinkedList<CSVEntry> entries;
+
+    public CSVReader(){
+        this.entries = new LinkedList<CSVEntry>();
+    }
+
+    public void readCSV(String path){
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        try {
+
+            br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null) {
+
+                String[] lineRead = line.split(cvsSplitBy);
+
+                this.entries.add(new CSVEntry(Long.valueOf(lineRead[0]),Integer.valueOf(lineRead[1])));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/agh/eis/mralucp/beacontrilateration/DistanceRSSIConverter.java
+++ app/src/main/java/com/agh/eis/mralucp/beacontrilateration/DistanceRSSIConverter.java
@@ -4,4 +4,14 @@ package com.agh.eis.mralucp.beacontrilateration;
  * Created by Lucp on 2015-01-27.
  */
 public class DistanceRSSIConverter {
+
+    private static double A = -60;
+    private static double n = 2.2;
+
+    public static double convertDistance(int rssi){
+        double dis = Math.pow(10, ((A-(double)rssi)/10*n));
+        return dis;
+    }
+
+
 }
