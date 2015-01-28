904d7121f02a182bc9cca32955a3e1b800a81d63
trzytematyczna <monikrakoczy@gmail.com>
2015-01-28 22:49:27 +0100

    breceiver + services changes

--- app/src/main/java/com/agh/eis/mralucp/beacontrilateration/DataReceiver.java
+++ app/src/main/java/com/agh/eis/mralucp/beacontrilateration/DataReceiver.java
@@ -12,6 +12,7 @@ public class DataReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
+
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
--- app/src/main/java/com/agh/eis/mralucp/beacontrilateration/EvaluationService.java
+++ app/src/main/java/com/agh/eis/mralucp/beacontrilateration/EvaluationService.java
@@ -1,11 +1,28 @@
 package com.agh.eis.mralucp.beacontrilateration;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 
 public class EvaluationService extends Service {
+
+    private BroadcastReceiver mDataReceiver;
+
     public EvaluationService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.mDataReceiver = new DataReceiver();
+        this.registerReceiver(this.mDataReceiver, new IntentFilter("com.aware.plugin.beacons.SCAN_RESULT_ACTION"));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
@@ -13,4 +30,8 @@ public class EvaluationService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+
+
+
 }
