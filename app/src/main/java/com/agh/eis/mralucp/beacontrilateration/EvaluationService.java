e8ac2d1629d3c6a6b55ea87f5bd3ea648b3c487a
trzytematyczna <monikrakoczy@gmail.com>
2015-01-28 17:35:12 +0100

    receiver /service

---
+++ app/src/main/java/com/agh/eis/mralucp/beacontrilateration/EvaluationService.java
@@ -0,0 +1,16 @@
+package com.agh.eis.mralucp.beacontrilateration;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class EvaluationService extends Service {
+    public EvaluationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
