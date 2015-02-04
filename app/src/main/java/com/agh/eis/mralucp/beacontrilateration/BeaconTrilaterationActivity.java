86d7d075f2ad7737be4be726c46fd2de3f69ba54
trzytematyczna <monikrakoczy@gmail.com>
2015-02-04 22:20:22 +0100

    service schema bind

--- app/src/main/java/com/agh/eis/mralucp/beacontrilateration/BeaconTrilaterationActivity.java
+++ app/src/main/java/com/agh/eis/mralucp/beacontrilateration/BeaconTrilaterationActivity.java
@@ -1,5 +1,10 @@
 package com.agh.eis.mralucp.beacontrilateration;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,11 +17,37 @@ public class BeaconTrilaterationActivity extends ActionBarActivity {
 
     private Button showPathButton;
 
+    EvaluationService mService;
+    boolean isBound = false;
+
+    private ServiceConnection myConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            EvaluationService.MyLocalBinder binder = (EvaluationService.MyLocalBinder) service;
+            mService = binder.getService();
+            isBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName arg0) {
+            isBound = false;
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon_trilateration);
+        Intent intent = new Intent(this, EvaluationService.class);
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
         this.showPathButton = (Button) findViewById(R.id.show_path_button);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
@@ -24,6 +55,12 @@ public class BeaconTrilaterationActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_beacon_trilateration, menu);
         return true;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(myConnection);
     }
 
     @Override
--- app/src/main/java/com/agh/eis/mralucp/beacontrilateration/EvaluationService.java
+++ app/src/main/java/com/agh/eis/mralucp/beacontrilateration/EvaluationService.java
@@ -5,42 +5,79 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Binder;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import com.agh.eis.mralucp.beacontrilateration.handlers.DistanceRSSIConverter;
+import com.agh.eis.mralucp.beacontrilateration.model.Beacon;
+import com.agh.eis.mralucp.beacontrilateration.model.Point;
 
 public class EvaluationService extends Service {
 
+    private final IBinder myBinder = new MyLocalBinder();
+
     private BroadcastReceiver mDataReceiver;
 
+    public class MyLocalBinder extends Binder {
+        EvaluationService getService() {
+            return EvaluationService.this;
+        }
+    }
     public EvaluationService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return Service.START_NOT_STICKY;
+//        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Toast.makeText(this.getApplicationContext(), "onCreate Service ", Toast.LENGTH_LONG);//+signal+" "+obj, Toast.LENGTH_LONG);
+
         this.mDataReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent){
-                returnRSSI(intent.getIntExtra("RSSI", 0));
+//                int signal  = returnRSSI(intent.getIntExtra("RSSI", 0));
+//                Object beaconUID = intent.getSerializableExtra("UUID");
+//                Beacon beacon = findBeacon(beaconUID);
+//                if(beacon!=null){
+//                  beforeTrilateration(beacon, signal);
+//
+//                }
+                Toast.makeText(context, "broadcast ", Toast.LENGTH_LONG);//+signal+" "+obj, Toast.LENGTH_LONG);
             }
         };
         this.registerReceiver(this.mDataReceiver, new IntentFilter("com.aware.plugin.beacons.SCAN_RESULT_ACTION"));
     }
 
-    private void returnRSSI(int rssi) {
+    private Point beforeTrilateration(Beacon beacon, double signal){
+//                dopisanie do historii do beacona
+//                wywolanie funkcji csvhandler
+//                zwraca punkt lub nulla
+        return null;
+    }
+    private double returnRSSI(int rssi) {
+        return DistanceRSSIConverter.convertDistance(rssi);
     }
 
     @Override
     public void onDestroy() {
+        unregisterReceiver(mDataReceiver);
         super.onDestroy();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return myBinder;
     }
 
+    @Override
+    public boolean onUnbind(Intent intent) {
 
-
-
+        return super.onUnbind(intent);
+    }
 }
